c612222b431b718c44eceada6a02e407414e425c
Andrey <gerasimec.by@mail.ru>
2024-04-24 23:00:02 +0300

    Revert "Update main.py"

--- Howework/3_2.py
+++ Howework/3_2.py
@@ -1,7 +1,13 @@
 # Пользователь вводит 3 числа, найти среднее арифмитическое с точность 3
 
-a = 33
+"""a = 33
 b = 45
 c = 65
+d = (round((a + b +c)/3, 3))
+print(d)"""
+
+a = int(input('enter integer: '))
+b = int(input('enter integer: '))
+c = int(input('enter integer: '))
 d = (round((a + b +c)/3, 3))
 print(d)
