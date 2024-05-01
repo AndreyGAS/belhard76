d29cd717e83131c8f09e5e8ce9f6f4dcf1d60ee2
Andrey <gerasimec.by@mail.ru>
2024-05-01 22:09:20 +0300

    Revert "Update main.py"

--- Howework/04/4_2.py
+++ Howework/04/4_2.py
@@ -1,7 +1,19 @@
-a = input("введите текст: ")
+# Без использования collections, написать программу, которая будет создавать словарь
+# для подсчитывания количества вхождений каждойбуквы в текст введенный с клавиатуры
+
+"""a = input("введите текст: ")
 dictionary = {}
 for letter in a:
     if letter not in dictionary:
         dictionary[letter] = 0
     dictionary[letter] += 1
-print(dictionary)
+print(dictionary)"""
+
+"""text = input()
+symbol_counter = {i: text.count(i) for i in text}
+print(symbol_counter)"""
+
+user_input = input("введите текст: ")
+temp_set = set(user_input) #какие символы в строке
+output_dict = {symbol: user_input.count(symbol) for symbol in temp_set}
+print(output_dict)
